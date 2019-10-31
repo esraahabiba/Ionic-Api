f4c5972d9d5364f2c8156093e6c7c4cab018449f
esraahabiba <esraahabiba2018@gmail.com>
2019-10-31 15:20:42 +0200

    API

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs/internal/Observable';
 
 @Component({
   selector: 'app-tab1',
@@ -6,7 +8,38 @@ import { Component } from '@angular/core';
   styleUrls: ['tab1.page.scss']
 })
 export class Tab1Page {
+  clc: Observable<any>;
+  constructor(public httpClient: HttpClient) {
+  /*  this.clc = this.httpClient.get('http://192.168.1.100:3000/calc/body');
+    this.clc.subscribe(data => {
+      console.log('First API : ', data);
+    }
+    )*/
 
-  constructor() {}
 
+  }
+ /* httpNodeCors: {
+    origin: "*",
+    methods: "GET,PUT,POST,DELETE"
+   }*/
+num1 :Number ; 
+num2 :Number ; 
+  sendPostRequest(num1 , num2) {
+    this.num1 = num1 
+    this.num2 = num2 
+   /* let postData = {
+            "name": "Customer004",
+            "email": "customer004@email.com",
+            "tel": "0000252525"
+    }*/
+    let data ={
+     "num1":num1 ,
+   "num2":num2  
+    }
+
+    this.httpClient.get("http://192.168.1.100:3000/calc/body?num1="+num1+"&num2="+num2).subscribe(data => {console.log(data['_body']);
+       }, error => {
+        console.log(error);
+      });
+  }
 }
